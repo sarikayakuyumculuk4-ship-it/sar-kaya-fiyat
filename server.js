d1a721dddf2dc6db301c7fdc194653ae837f1c0f
sarikayakuyumculuk4-ship-it <sarikayakuyumculuk4@gmail.com>
2026-02-21 15:11:57 +0300

    Update server.jsnew

--- server.js
+++ server.js
@@ -8,23 +8,25 @@ app.use(cors());
 
 const PORT = process.env.PORT || 10000;
 
+// TEST
+app.get("/", (req, res) => {
+  res.send("Backend çalışıyor");
+});
+
 app.get("/api/harem", async (req, res) => {
   try {
     const response = await fetch("https://www.haremaltin.com");
     const html = await response.text();
-
     const $ = cheerio.load(html);
 
-    // SAYFADAKİ GRAM ALTIN SATIRINI BUL
     const gramSatir = $("td:contains('Gram Altın')").parent();
-
     const gramAlis = gramSatir.find("td").eq(1).text().trim();
     const gramSatis = gramSatir.find("td").eq(2).text().trim();
 
     res.json({
       gramAltin: {
-        alis: gramAlis,
-        satis: gramSatis
+        alis: gramAlis || "bulunamadı",
+        satis: gramSatis || "bulunamadı"
       }
     });
 
